d1d4e9bad8f67b59e1b07b8f487845f044b7613f
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-09-13 21:19:45 -0400

    commit tree 80250a3dacc1011b3ad588f86e617c3725316365

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+} 
+
+const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(string="*") {
+    return function(string1="special") {
+        return `You are ${string}${string1}${string}!`
+    }
+
+}
+
+const Calculator = new Object();
+
+Calculator.add = function(a,b) {
+    return a + b;
+}
+
+Calculator.subtract = function(a,b) {
+    return a - b;
+}
+
+Calculator.multiply = function(a,b) {
+    return a * b;
+}
+
+Calculator.divide = function(a,b) {
+    return a / b;
+}
+
+function actionApplyer(int, array) {
+    for (let i = 0; i < array.length; i++) {
+        int = array[i](int)
+    }
+    return int
+}
